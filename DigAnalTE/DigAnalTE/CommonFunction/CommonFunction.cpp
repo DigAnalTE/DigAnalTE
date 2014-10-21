a3a8a063d7eee9c4f769f6c8fb29ab7f1f1a4b03
DigAnalTE <diganalte@gmail.com>
2014-10-22 07:05:47 +0800

    对函数ReplaceName进行了修改

--- DigAnalTE/DigAnalTE/CommonFunction/CommonFunction.cpp
+++ DigAnalTE/DigAnalTE/CommonFunction/CommonFunction.cpp
@@ -11,24 +11,29 @@ bool ReplaceName(char* tName, int Length)
 	char *ex;
 	int i, j;
 	//trimleft
-	ex = tName; i = 0;
-	while ((*ex == ' ' || *ex == 9) && i < Length - 1){ ex++; i++; }//找到开始的非空字符
-	for (j = 0; j < Length - 1 && i < Length - 1; j++, ex++, i++)
+	if (tName[0] == ' ' || tName[0] == 9)
 	{
-		if (*ex == '\0')break;
-		tName[j] = *ex;
-	}
-	tName[j] = '\0';
-	//trimright
-	j--;
-	for (; j >= 0; j--)
-	{
-		if (tName[j] == ' ' || tName[j] == '\r' || tName[j] == '\n' || tName[j] == 9)
+		ex = tName; i = 0;
+		while ((*ex == ' ' || *ex == 9) && i < Length - 1){ ex++; i++; }//找到开始的非空字符
+		for (j = 0; j < Length - 1 && i < Length - 1; j++, ex++, i++)
 		{
-			tName[j] = '\0';
-			continue;
+			if (*ex == '\0')break;
+			tName[j] = *ex;
 		}
-		break;
+		tName[j] = '\0';
+	}
+	//trimright
+	j = strlen(tName);
+	if (j >= Length)
+	{
+		tName[Length - 1] = '\0';
+		j = strlen(tName);
+	}
+	j--;
+	while (tName[j] == ' ' || tName[j] == '\r' || tName[j] == '\n' || tName[j] == 9)
+	{
+		tName[j] = '\0';
+		j--;
 	}
 	return true;
 }
